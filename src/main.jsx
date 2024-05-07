4e8b4b6a581896ac816867e5c2267cce91b6af19
francisco-solis99 <franciscosoliscss@gmail.com>
2024-05-06 20:20:04 -0600

    [cheat] add chakra provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+import { ChakraProvider } from '@chakra-ui/react';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
 );
