defe5384a342f36f036f22d843bb48a4b2900522
francisco-solis99 <franciscosoliscss@gmail.com>
2024-05-06 20:19:29 -0600

    [chore] add menu with their items

---
+++ src/components/Menu.jsx
@@ -0,0 +1,15 @@
+
+export default function Menu() {
+  return (
+    <aside className='aside__menu'>
+      <h1>TODOs</h1>
+      <nav>
+        <ul>
+          <li><a href="">Home</a></li>
+          <li><a href="">Tasks</a></li>
+          <li><a href="">Lists</a></li>
+        </ul>
+      </nav>
+    </aside>
+  );
+}
