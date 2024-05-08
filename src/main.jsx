ed73ab6ea3e035e33c86235e04f444624b5b8715
francisco-solis99 <franciscosoliscss@gmail.com>
2024-05-07 18:15:28 -0600

    [chore] use the tasks provider as wrapper

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { ChakraProvider } from '@chakra-ui/react';
+import { TasksProvider } from './context/TasksContext';
+
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -9,7 +11,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ChakraProvider>
-      <App />
+      <TasksProvider>
+        <App />
+      </TasksProvider>
     </ChakraProvider>
   </React.StrictMode>,
 );
