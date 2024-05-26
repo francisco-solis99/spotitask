ec0efee0a7d7d90c2ce1769f1e1d90fcd5161c15
francisco-solis99 <franciscosoliscss@gmail.com>
2024-05-25 23:06:51 -0600

    [chore] use the delete action inside of the list detail

--- src/pages/ListDetail.tsx
+++ src/pages/ListDetail.tsx
@@ -1,19 +1,27 @@
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 
-import { Box, Button, Flex, Heading, Spacer } from '@chakra-ui/react';
+import { Box, Flex, Heading, Spacer } from '@chakra-ui/react';
 
 import Container from '../components/Container';
 import AddListButton from '../components/AddListButton';
 import AddButton from '../components/AddButton';
 import ListTasks from '../components/Tasks/ListTasks';
-import { RemoveIcon } from '../components/icons/RemoveIcon';
 
 import { useListsTasks } from '../hooks/useListsTasks';
+import { useListsTasksContext } from '../hooks/useListTasksContext';
+import DeleteButtonList from '../components/DeleteButtonList';
 
 
 export default function ListDetail() {
   const { listId } = useParams()
   const { listByListId: list } = useListsTasks({ listId })
+  const { deleteList } = useListsTasksContext()
+  const navigate = useNavigate();
+
+  const deleteListAction = () => {
+    deleteList({ idList: list.id })
+    navigate('/lists')
+  }
 
   return (
     <div>
@@ -22,23 +30,15 @@ export default function ListDetail() {
           <Flex align={'center'}>
             <Heading as="h1" size="xl" >{list.name}({list.tasks.length})</Heading>
             <Spacer />
-            <Button
-              leftIcon={<RemoveIcon />}
-              bg={'transparent'}
-              color={'red.300'}
-              variant="ghost"
-              _hover={{ color: 'red.500' }}
-              title="Delete list"
-            >Delete
-            </Button>
+            <DeleteButtonList deleteListCb={deleteListAction} />
           </Flex>
           <Box maxW='5xl' marginBlock="2em">
             <ListTasks tasks={list.tasks} />
           </Box>
-          <Box style={{ position: "fixed", bottom: "5%", right: "5%" }}>
-            <AddListButton />
+          <Flex flexDirection={"column"} gap={'.75em'} style={{ position: "fixed", bottom: "5%", right: "5%" }}>
             <AddButton />
-          </Box>
+            <AddListButton />
+          </Flex>
         </Box>
       </Container>
     </div>
