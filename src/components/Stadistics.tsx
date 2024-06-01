bb757e775d7692dac96d58744bc1fc564e0b2e7f
francisco-solis99 <franciscosoliscss@gmail.com>
2024-05-31 19:46:16 -0600

    [refactor] separete the stadistic section into a component

---
+++ src/components/Stadistics.tsx
@@ -0,0 +1,53 @@
+import {
+  CircularProgress, CircularProgressLabel, Stat,
+  StatLabel,
+  StatNumber,
+  StatHelpText,
+  Grid,
+  Tag,
+} from '@chakra-ui/react'
+import { useEffect, useState } from 'react'
+
+export default function Stadistics({ total, rest }: { total: number, rest: number }) {
+  const doneTasksPorcentaje = Number(((rest * 100) / total).toFixed(1))
+  const [progressColor, setProgressColor] = useState('white')
+
+  useEffect(() => {
+    if (doneTasksPorcentaje <= 20) setProgressColor('red')
+    else if (doneTasksPorcentaje > 20 && doneTasksPorcentaje < 60) setProgressColor('yellow')
+    else setProgressColor('green')
+  }, [rest])
+
+
+  return (
+    <>
+      <Grid templateColumns='repeat(3, 1fr)' justifyItems={'center'} alignItems={'center'} gap={6}>
+        <Stat>
+          <StatLabel>Total Tasks</StatLabel>
+          <StatNumber textAlign={'center'}>
+            <Tag marginBlockStart={'.25em'} size={'lg'} variant='solid' colorScheme='teal'>
+              {total}
+            </Tag>
+          </StatNumber>
+          <StatHelpText></StatHelpText>
+        </Stat>
+        <Stat>
+          <StatLabel>Tasks Done</StatLabel>
+          <StatNumber textAlign={'center'}>
+            <Tag marginBlockStart={'.25em'} size={'lg'} variant='solid' colorScheme='green'>
+              {rest}
+            </Tag>
+          </StatNumber>
+          <StatHelpText></StatHelpText>
+        </Stat>
+        <Stat>
+          <StatLabel>Tasks Progress</StatLabel>
+          <CircularProgress size='80px' thickness='7px' value={doneTasksPorcentaje} color={`${progressColor}.400`}>
+            <CircularProgressLabel fontSize={'1rem'}>{doneTasksPorcentaje}%</CircularProgressLabel>
+          </CircularProgress>
+        </Stat>
+      </Grid>
+
+    </>
+  )
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,5 @@
-import { useRef } from "react";
 import {
-  Box, CircularProgress, CircularProgressLabel, Stat,
-  StatLabel,
-  StatNumber,
-  StatHelpText,
-  Grid,
-  Tag,
+  Box,
 } from '@chakra-ui/react'
 
 import Container from "../components/Container";
@@ -14,50 +8,18 @@ import ActionsButtons from "../components/ActionsButtons";
 
 import { useTasks } from "../hooks/useTasks";
 import { Task } from "../types/types";
+import Stadistics from "../components/Stadistics";
 
 export default function Home() {
   const { tasks } = useTasks({ querySearch: '' });
-
-  const numTasksDone: number = tasks.filter((task: Task) => task.done).length
-  const doneTasksPorcentaje = Number(((numTasksDone * 100) / tasks.length).toFixed(1))
-  const progressColor = useRef('')
-
-  if (doneTasksPorcentaje <= 20) progressColor.current = 'red'
-  else if (doneTasksPorcentaje > 20 && doneTasksPorcentaje < 60) progressColor.current = 'yellow'
-  else progressColor.current = 'green'
-
+  const numTasksDone = tasks.filter((task: Task) => task.done).length;
 
   return (
     <>
       <Container>
         <Box maxW='5xl'>
           {/* Statdistics */}
-          <Grid templateColumns='repeat(3, 1fr)' justifyItems={'center'} alignItems={'center'} gap={6}>
-            <Stat>
-              <StatLabel>Total Tasks</StatLabel>
-              <StatNumber textAlign={'center'}>
-                <Tag marginBlockStart={'.25em'} size={'lg'} variant='solid' colorScheme='teal'>
-                  {tasks.length}
-                </Tag>
-              </StatNumber>
-              <StatHelpText></StatHelpText>
-            </Stat>
-            <Stat>
-              <StatLabel>Tasks Done</StatLabel>
-              <StatNumber textAlign={'center'}>
-                <Tag marginBlockStart={'.25em'} size={'lg'} variant='solid' colorScheme='green'>
-                  {numTasksDone}
-                </Tag>
-              </StatNumber>
-              <StatHelpText></StatHelpText>
-            </Stat>
-            <Stat>
-              <StatLabel>Tasks Progress</StatLabel>
-              <CircularProgress size='80px' thickness='7px' value={doneTasksPorcentaje} color={`${progressColor.current}.400`}>
-                <CircularProgressLabel fontSize={'1rem'}>{doneTasksPorcentaje}%</CircularProgressLabel>
-              </CircularProgress>
-            </Stat>
-          </Grid>
+          <Stadistics total={tasks.length} rest={numTasksDone} />
 
           {/* Tasks */}
           <Box marginBlock={'2em'}>
